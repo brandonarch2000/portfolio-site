659136a7092a62354cb009a13b66a606586baac1
Brandon Archibald <brandonarch2000@gmail.com>
2026-02-13 16:28:19 -0600

    update next.config.ts to work with git hub pages

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site/',
 };
-module.exports = nextConfig;
+
+export default nextConfig;
